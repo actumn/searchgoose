627e872b7ac75f80fee8cfbd9aaa1d11b43ac23a
emiling <scopradical@gmail.com>
2020-09-03 12:16:15 +0900

    Add skeleton code of TCP connection and handskaing

--- main.go
+++ main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"github.com/actumn/searchgoose/http"
 	"github.com/actumn/searchgoose/state/cluster"
-	"github.com/actumn/searchgoose/state/discovery"
-	"github.com/actumn/searchgoose/state/indices"
-	"github.com/actumn/searchgoose/state/metadata"
-	"github.com/actumn/searchgoose/state/persist"
-	"github.com/actumn/searchgoose/state/transport"
+	"github.com/actumn/searchgoose/state/transport/tcp"
 	"log"
+	"net"
+	"time"
 )
 
 func main() {
@@ -16,35 +13,64 @@ func main() {
 }
 
 func start() {
+
 	nodeId := cluster.GenerateNodeId()
+	transport := tcp.NewTransport(nodeId)
 
-	transportService := transport.NewService(nodeId)
-	clusterService := cluster.NewService()
-	persistClusterStateService := persist.NewClusterStateService()
+	// TODO :: Handshake 로직은 어디에서 관리되어야 할까
 
-	gateway := metadata.NewGatewayMetaState()
-	gateway.Start(transportService, clusterService, persistClusterStateService)
+	log.Printf("[Node Id] : %s\n", nodeId)
 
-	coordinator := discovery.NewCoordinator(transportService, clusterService.ApplierService, clusterService.MasterService, gateway.PersistedState)
+	// TODO :: 현재 노드의 host와 port 설정을 가져오게 하자
+	//port := "8179"
+	//port := "8180"
+	port := "8181"
 
-	indicesService := indices.NewService()
-	indicesClusterStateService := indices.NewClusterStateService(indicesService)
+	go transport.Start(port)
+	time.Sleep(time.Duration(15) * time.Second)
 
-	clusterService.ApplierService.AddApplier(indicesClusterStateService.ApplyClusterState)
-	clusterService.MasterService.ClusterStatePublish = coordinator.Publish
+	//seedHosts := []string{"localhost:8180", "localhost:8181"} //8179
+	//seedHosts := []string{"localhost:8179", "localhost:8181"} //8180
+	seedHosts := []string{"localhost:8179", "localhost:8180"} //8181
 
-	clusterMetadataCreateIndexService := cluster.NewMetadataCreateIndexService(clusterService)
+	log.Printf("Start handshaking\n")
 
-	gateway.Start(
-		transportService,
-		clusterService,
-		persistClusterStateService,
-	)
-	coordinator.Start()
-
-	b := http.New(clusterService, clusterMetadataCreateIndexService)
-	log.Println("start server...")
-	if err := b.Start(); err != nil {
-		panic(err)
+	connections := make(chan *net.Conn)
+	for _, seedHost := range seedHosts {
+		// Open connection
+		transport.OpenConnection(seedHost, connections)
 	}
+
+	// Handshake 동작 보기 위해서 잠시 주석 처리
+	/*
+		transportService := transport.NewService(nodeId)
+		clusterService := cluster.NewService()
+		persistClusterStateService := persist.NewClusterStateService()
+
+		gateway := metadata.NewGatewayMetaState()
+		gateway.Start(transportService, clusterService, persistClusterStateService)
+
+		coordinator := discovery.NewCoordinator(transportService, clusterService.ApplierService, clusterService.MasterService, gateway.PersistedState)
+
+		indicesService := indices.NewService()
+		indicesClusterStateService := indices.NewClusterStateService(indicesService)
+
+		clusterService.ApplierService.AddApplier(indicesClusterStateService.ApplyClusterState)
+		clusterService.MasterService.ClusterStatePublish = coordinator.Publish
+
+		clusterMetadataCreateIndexService := cluster.NewMetadataCreateIndexService(clusterService)
+
+		gateway.Start(
+			transportService,
+			clusterService,
+			persistClusterStateService,
+		)
+		coordinator.Start()
+
+		b := http.New(clusterService, clusterMetadataCreateIndexService)
+		log.Println("start server...")
+		if err := b.Start(); err != nil {
+			panic(err)
+		}
+	*/
 }
--- state/transport/tcp/tcp_transport.go
+++ state/transport/tcp/tcp_transport.go
@@ -8,13 +8,26 @@ import (
 )
 
 type Transport struct {
+	//localConnection *net.Conn
+	LocalNodeId    string
+	ConnectedNodes map[string]*net.Conn // nodeId -> Connection
 }
 
-func (t *Transport) Start() {
-	l, err := net.Listen("tcp", ":8180")
-	if err != nil {
-		log.Fatalf("Fail to bind address to 8180; err: %v", err)
+func NewTransport(nodeId string) *Transport {
+	return &Transport{
+		LocalNodeId:    nodeId,
+		ConnectedNodes: map[string]*net.Conn{},
 	}
+}
+
+func (t *Transport) Start(srcAddress string) {
+
+	l, err := net.Listen("tcp", ":"+srcAddress)
+	// l, err := net.Listen("tcp", ":8180")
+	if err != nil {
+		log.Fatalf("Fail to bind address to %s; err: %v", srcAddress, err)
+	}
+	log.Printf("Success of listening on %s\n", srcAddress)
 	defer l.Close()
 
 	for {
@@ -36,21 +49,57 @@ func (t *Transport) Start() {
 				return
 			}
 			if 0 < n {
+				// TODO :: 만약 target node로부터 ACK가 먼저 들어온다면?
 				data := recvBuf[:n]
-				log.Println(string(data))
+				log.Printf("Recived ACK from %s", string(data))
+				// TODO :: 여기서 어떻게 main에 있는 ConnectedNodes에 연결시키지?
+				// remoteNodeId := string(data)
+				// t.ConnectedNodes[remoteNodeId] = &conn
+
 			}
 		}(conn)
 	}
 }
 
-func (t *Transport) Send() {
-	conn, err := net.Dial("tcp", "lcoalhost:8180")
+func (t *Transport) Send(destAddress string, message []byte) {
+	conn, err := net.Dial("tcp", destAddress)
 	if err != nil {
-		log.Fatalf("Failed to connect to server", err)
+		log.Fatalf("Failed to connect to %s : %v", destAddress, err)
 	}
 
 	for {
-		conn.Write([]byte("ping"))
+		//conn.Write([]byte("ping"))
+		conn.Write(message)
 		time.Sleep(time.Duration(3) * time.Second)
 	}
+}
+
+func (t *Transport) OpenConnection(destAddress string, c chan *net.Conn) {
+	conn, err := net.Dial("tcp", destAddress)
+	if err != nil {
+		log.Fatalf("Failed to connect to %s : %v", destAddress, err)
+	}
+	log.Printf("Success on connecting %s\n", destAddress)
+	connectedNodes := make(chan string)
+	// TODO :: ACK를 받을 때 까지 계속 보내? 아니지 1분 정도는 connection 시도를 하자
+	go t.HandShake(conn, connectedNodes)
+	time.Sleep(time.Duration(10) * time.Second)
+	//c <- &conn
+
+}
+
+func (t *Transport) HandShake(conn net.Conn, connectedNodes chan<- string) {
+	for {
+		//conn.Write([]byte("ping"))
+		conn.Write([]byte(t.LocalNodeId))
+		time.Sleep(time.Duration(3) * time.Second)
+	}
+}
+
+type OpenConnectionRequest struct {
+	NodeId string
+}
+
+type OpenConnectionResponse struct {
+	NodeId string
 }
